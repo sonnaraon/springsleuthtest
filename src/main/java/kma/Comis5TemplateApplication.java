92887120c41b03f096e3f1cd60fa48b23148e448
Changwan <Changwan@DESKTOP-DD4DFAA.lgcns.com>
2019-07-05 19:36:26 +0900

    123

--- src/main/java/kma/Comis5TemplateApplication.java
+++ src/main/java/kma/Comis5TemplateApplication.java
@@ -1,11 +1,14 @@
 package kma;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ImportResource("classpath:applicationContext.xml")
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class Comis5TemplateApplication {
 
 	public static void main(String[] args) {
