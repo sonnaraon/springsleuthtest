6e3f250a2a1d2a72bd2485970415b86b68852901
nrson <nrson@nrson-PC.lgcns.com>
2019-08-29 16:05:55 +0900

    Spring Sleuth Sample!

--- src/main/java/com/spring/sleuth/ServletInitializer.java
+++ src/main/java/com/spring/sleuth/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SleuthTemplateApplication.class);
+		return application.sources(ElkExampleSpringBootApplication.class);
 	}
 
 }
