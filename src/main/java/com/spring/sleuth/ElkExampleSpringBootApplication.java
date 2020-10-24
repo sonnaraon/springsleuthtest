37f14195ab547e37a06eb5c4ad4caa9b9fd6dcca
nara0617 <nara0617@gmail.com>
2020-10-24 22:50:34 +0900

    test

--- src/main/java/com/spring/sleuth/ElkExampleSpringBootApplication.java
+++ src/main/java/com/spring/sleuth/ElkExampleSpringBootApplication.java
@@ -1,9 +1,12 @@
 package com.spring.sleuth;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class ElkExampleSpringBootApplication {
 
 	public static void main(String[] args) {
