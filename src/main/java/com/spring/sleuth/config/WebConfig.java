4e1f9d494b6b5d0b46f94af7ed1c78b4af399f33
nara0617 <nara0617@gmail.com>
2020-10-25 10:53:53 +0900

    test

--- src/main/java/com/spring/sleuth/config/WebConfig.java
+++ src/main/java/com/spring/sleuth/config/WebConfig.java
@@ -6,8 +6,6 @@ import javax.servlet.Filter;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
@@ -17,11 +15,10 @@ import com.amazonaws.xray.AWSXRayRecorderBuilder;
 import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter;
 import com.amazonaws.xray.plugins.EC2Plugin;
 import com.amazonaws.xray.plugins.ElasticBeanstalkPlugin;
-import com.amazonaws.xray.strategy.sampling.LocalizedSamplingStrategy;
+import com.amazonaws.xray.strategy.sampling.CentralizedSamplingStrategy;
 
 @Configuration
 public class WebConfig implements ServletContextListener {
-	private static final Logger logger = LoggerFactory.getLogger(WebConfig.class);
 
 	@Bean
 	MappingJackson2JsonView jsonView() {
@@ -38,7 +35,7 @@ public class WebConfig implements ServletContextListener {
     AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder.standard().withPlugin(new EC2Plugin()).withPlugin(new ElasticBeanstalkPlugin());
 
     URL ruleFile = WebConfig.class.getResource("/sampling-rules.json");
-    builder.withSamplingStrategy(new LocalizedSamplingStrategy(ruleFile));
+    builder.withSamplingStrategy(new CentralizedSamplingStrategy(ruleFile));
 
     AWSXRay.setGlobalRecorder(builder.build());
 
