372861f602e2d3f1fc0a6079b2e8e06ec7862c30
nara0617 <nara0617@gmail.com>
2020-10-25 11:02:43 +0900

    test

--- src/main/java/com/spring/sleuth/config/WebConfig.java
+++ src/main/java/com/spring/sleuth/config/WebConfig.java
@@ -6,6 +6,9 @@ import javax.servlet.Filter;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
@@ -16,10 +19,14 @@ import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter;
 import com.amazonaws.xray.plugins.EC2Plugin;
 import com.amazonaws.xray.plugins.ElasticBeanstalkPlugin;
 import com.amazonaws.xray.strategy.sampling.CentralizedSamplingStrategy;
+import com.spring.sleuth.sample.service.ElkExampleService;
 
 @Configuration
 public class WebConfig implements ServletContextListener {
-
+	private static final Logger logger = LoggerFactory.getLogger(WebConfig.class);
+	@Autowired
+	ElkExampleService elsService;
+	
 	@Bean
 	MappingJackson2JsonView jsonView() {
 		return new MappingJackson2JsonView();
@@ -31,16 +38,21 @@ public class WebConfig implements ServletContextListener {
 	}
 
 	@Override
-  public void contextInitialized(ServletContextEvent event) {
-    AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder.standard().withPlugin(new EC2Plugin()).withPlugin(new ElasticBeanstalkPlugin());
+	public void contextInitialized(ServletContextEvent event) {
+		AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder.standard().withPlugin(new EC2Plugin())
+				.withPlugin(new ElasticBeanstalkPlugin());
 
-    URL ruleFile = WebConfig.class.getResource("/sampling-rules.json");
-    builder.withSamplingStrategy(new CentralizedSamplingStrategy(ruleFile));
+		URL ruleFile = WebConfig.class.getResource("/sampling-rules.json");
+		builder.withSamplingStrategy(new CentralizedSamplingStrategy(ruleFile));
 
-    AWSXRay.setGlobalRecorder(builder.build());
+		AWSXRay.setGlobalRecorder(builder.build());
 
-//    AWSXRay.beginSegment("springboot");
-//    
-//    AWSXRay.endSegment();
-  }
+		AWSXRay.beginSegment("springboot");
+		try {
+			elsService.elkServiceMethod();
+		} catch (Exception e) {
+			logger.warn("Failed!!");
+		}
+		AWSXRay.endSegment();
+	}
 }
