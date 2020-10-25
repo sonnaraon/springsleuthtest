6cc43076703377c1bcdf9c97731793bd7a0fbd0b
nara0617 <nara0617@gmail.com>
2020-10-25 10:45:24 +0900

    test

--- src/main/java/com/spring/sleuth/config/WebConfig.java
+++ src/main/java/com/spring/sleuth/config/WebConfig.java
@@ -3,6 +3,8 @@ package com.spring.sleuth.config;
 import java.net.URL;
 
 import javax.servlet.Filter;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,21 +19,22 @@ import com.amazonaws.xray.plugins.EC2Plugin;
 import com.amazonaws.xray.plugins.ElasticBeanstalkPlugin;
 import com.amazonaws.xray.strategy.sampling.LocalizedSamplingStrategy;
 
-
 @Configuration
-public class WebConfig {
-  private static final Logger logger = LoggerFactory.getLogger(WebConfig.class);
+public class WebConfig implements ServletContextListener {
+	private static final Logger logger = LoggerFactory.getLogger(WebConfig.class);
 
-  @Bean
+	@Bean
 	MappingJackson2JsonView jsonView() {
 		return new MappingJackson2JsonView();
 	}
-  @Bean
-  public Filter TracingFilter() {
-    return new AWSXRayServletFilter("springboot");
-  }
 
-  static {
+	@Bean
+	public Filter TracingFilter() {
+		return new AWSXRayServletFilter("springboot");
+	}
+
+	@Override
+  public void contextInitialized(ServletContextEvent event) {
     AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder.standard().withPlugin(new EC2Plugin()).withPlugin(new ElasticBeanstalkPlugin());
 
     URL ruleFile = WebConfig.class.getResource("/sampling-rules.json");
