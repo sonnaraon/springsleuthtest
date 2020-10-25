75270041f530109d689b339a1ae703e732374d7f
nara0617 <nara0617@gmail.com>
2020-10-25 10:38:21 +0900

    test

--- src/main/java/com/spring/sleuth/config/WebConfig.java
+++ src/main/java/com/spring/sleuth/config/WebConfig.java
@@ -2,31 +2,45 @@ package com.spring.sleuth.config;
 
 import java.net.URL;
 
+import javax.servlet.Filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 import com.amazonaws.xray.AWSXRay;
 import com.amazonaws.xray.AWSXRayRecorderBuilder;
+import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter;
 import com.amazonaws.xray.plugins.EC2Plugin;
-import com.amazonaws.xray.plugins.ECSPlugin;
-import com.amazonaws.xray.plugins.EKSPlugin;
+import com.amazonaws.xray.plugins.ElasticBeanstalkPlugin;
 import com.amazonaws.xray.strategy.sampling.LocalizedSamplingStrategy;
+
 
 @Configuration
 public class WebConfig {
+  private static final Logger logger = LoggerFactory.getLogger(WebConfig.class);
 
-	@Bean
+  @Bean
 	MappingJackson2JsonView jsonView() {
 		return new MappingJackson2JsonView();
 	}
-	
-	static {
-	    AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder.standard().withPlugin(new EC2Plugin()).withPlugin(new ECSPlugin()).withPlugin(new EKSPlugin());
+  @Bean
+  public Filter TracingFilter() {
+    return new AWSXRayServletFilter("springboot");
+  }
 
-	    URL ruleFile = WebConfig.class.getResource("/sampling-rules.json");
-	    builder.withSamplingStrategy(new LocalizedSamplingStrategy(ruleFile));
+  static {
+    AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder.standard().withPlugin(new EC2Plugin()).withPlugin(new ElasticBeanstalkPlugin());
 
-	    AWSXRay.setGlobalRecorder(builder.build());
-	  }
+    URL ruleFile = WebConfig.class.getResource("/sampling-rules.json");
+    builder.withSamplingStrategy(new LocalizedSamplingStrategy(ruleFile));
+
+    AWSXRay.setGlobalRecorder(builder.build());
+
+//    AWSXRay.beginSegment("springboot");
+//    
+//    AWSXRay.endSegment();
+  }
 }
