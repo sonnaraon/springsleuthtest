d36c14c246870a34ed7738377fea9d667c175ed3
nara0617 <nara0617@gmail.com>
2020-10-25 10:22:52 +0900

    test

--- src/main/java/com/spring/sleuth/config/WebConfig.java
+++ src/main/java/com/spring/sleuth/config/WebConfig.java
@@ -1,8 +1,17 @@
 package com.spring.sleuth.config;
 
+import java.net.URL;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+import com.amazonaws.xray.AWSXRay;
+import com.amazonaws.xray.AWSXRayRecorderBuilder;
+import com.amazonaws.xray.plugins.EC2Plugin;
+import com.amazonaws.xray.plugins.ECSPlugin;
+import com.amazonaws.xray.plugins.EKSPlugin;
+import com.amazonaws.xray.strategy.sampling.LocalizedSamplingStrategy;
 
 @Configuration
 public class WebConfig {
@@ -11,4 +20,13 @@ public class WebConfig {
 	MappingJackson2JsonView jsonView() {
 		return new MappingJackson2JsonView();
 	}
+	
+	static {
+	    AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder.standard().withPlugin(new EC2Plugin()).withPlugin(new ECSPlugin()).withPlugin(new EKSPlugin());
+
+	    URL ruleFile = WebConfig.class.getResource("/sampling-rules.json");
+	    builder.withSamplingStrategy(new LocalizedSamplingStrategy(ruleFile));
+
+	    AWSXRay.setGlobalRecorder(builder.build());
+	  }
 }
